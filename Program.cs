71bd5f471099415716f97d7aedcc5df2527f563d
raulbuffon <raulbuffon1999@gmail.com>
2021-05-17 20:37:33 -0300

    Execute basic searchs using selenium

--- Program.cs
+++ Program.cs
@@ -11,13 +11,44 @@ namespace selenium_exercises
         {
             IWebDriver driver = new ChromeDriver();
 
-            var Url = "http://testing.todorvachev.com";
+            var BaseUrl = "http://testing.todorvachev.com";
 
-            driver.Navigate().GoToUrl(Url);
+            // Search using Selenium for the element Name
+            driver.Navigate().GoToUrl(BaseUrl + "/selectors/name/");
 
-            Thread.Sleep(3000);
+            IWebElement element = driver.FindElement(By.Name("myName"));
+
+            if(element.Displayed) GreenMessage("I can see the search by Name!");
+
+            // Search using Selenium for the element Id
+            driver.Navigate().GoToUrl(BaseUrl + "/selectors/id/");
+
+            element = driver.FindElement(By.Id("testImage"));
+
+            if(element.Displayed) GreenMessage("I can see the search by Id!");
+
+            // Search using Selenium for the element Class Name
+            driver.Navigate().GoToUrl(BaseUrl + "/selectors/class-name/");
+
+            element = driver.FindElement(By.ClassName("testClass"));
+
+            if(element.Displayed) GreenMessage($"I can see the search by Class Name! His text is \"{element.Text}\"");
 
             driver.Quit();
+        }
+
+        private static void RedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void GreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
