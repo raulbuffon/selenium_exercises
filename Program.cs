395f431ed9242c712d01f830bee401f3512b0cb0
raulbuffon <raulbuffon1999@gmail.com>
2021-05-20 11:24:15 -0300

    CSS and X path & Rxception handle for failed search

--- Program.cs
+++ Program.cs
@@ -34,6 +34,29 @@ namespace selenium_exercises
 
             if(element.Displayed) GreenMessage($"I can see the search by Class Name! His text is \"{element.Text}\"");
 
+            // Search using Selenium for the element CSS and X path
+            driver.Navigate().GoToUrl(BaseUrl + "/selectors/css-path/");
+
+            var cssPath = "#post-108 > div > figure > img";
+            var xPath = "//*[@id=\"post-108\"]/div/figure/img";
+
+            element = driver.FindElement(By.CssSelector(cssPath));
+            if(element.Displayed) GreenMessage($"I can see the search by Css Path");
+
+            element = driver.FindElement(By.XPath(xPath));
+            if(element.Displayed) GreenMessage($"I can see the search by X Path");
+            
+            // Exception handle if dont find the element in search
+            var wrongXPath = "//*[@id=\"post-1088888\"]/div/figure/img";
+            try
+            {   
+                element = driver.FindElement(By.XPath(wrongXPath));
+                if(element.Displayed) GreenMessage($"I can see the search by X Path");
+            }
+            catch(NoSuchElementException)
+            {
+                RedMessage($"I cannot see the search by X Path, something is wrong here ;-;");
+            }
             driver.Quit();
         }
 
